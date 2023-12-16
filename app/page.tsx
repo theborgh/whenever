fb990c605ed91a582a1cf65e4b4a452211987497
theborgh <borghiborghi@gmail.com>
2023-12-16 06:33:30 +0100

    implement form step 1 frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,10 @@
-import { Select } from '@mantine/core';
-import moment from 'moment-timezone';
-import DateSelector from '@/components/DateSelector/DateSelector';
-import styles from './page.module.css';
+import FormStepOne from '@/components/FormStepOne/FormStepOne';
 
 export default function HomePage() {
-  const timezones = moment.tz.names();
-
-  console.log(timezones);
-
   return (
     <section className="mt-8">
       <h2 className="text-2xl font-bold">Create a new meeting</h2>
-      <form>
-        <input className={styles.form__input} type="text" placeholder="Meeting name" />
-        <div className={styles.form__datetimeContainer}>
-          <div>
-            <div>Range of possible dates for the meeting</div>
-            <DateSelector />
-          </div>
-          <div>
-            <Select
-              label="Start time"
-              placeholder="Pick value"
-              data={[...Array.from({ length: 24 }).map((_, index) => `${index}:00`)]}
-            />
-            <Select
-              label="End time"
-              placeholder="Pick value"
-              data={[...Array.from({ length: 24 }).map((_, index) => `${index}:00`)]}
-            />
-          </div>
-          <div className={styles.form__datetimeContainer}>
-            <Select
-              label="Timezone"
-              placeholder="Type and select value"
-              data={[...timezones]}
-              searchable
-            />
-          </div>
-        </div>
-        <div className="flex justify-center">
-          <button type="submit" className={styles.form__submit}>
-            Create meeting
-          </button>
-        </div>
-      </form>
+      <FormStepOne />
     </section>
   );
 }
---
+++ components/FormStepOne/FormStepOne.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import React from 'react';
+import { Box, Select, TextInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { DatePicker } from '@mantine/dates';
+import moment from 'moment-timezone';
+import styles from './FormStepOne.module.css';
+import '@mantine/dates/styles.css';
+
+export default function FormStepOne() {
+  const timezones = moment.tz.names();
+
+  const form = useForm({
+    initialValues: {
+      dateRange: [new Date(), new Date(Date.now() + 6 * 24 * 60 * 60 * 1000)],
+      meetingName: 'New meeting',
+      startTime: '9:00',
+      endTime: '18:00',
+      timezone: moment.tz.guess(),
+    },
+
+    validate: {
+      meetingName: (value) => (value.length > 0 ? null : 'Meeting name is required'),
+      timezone: (value) => (timezones.includes(value) ? null : 'Invalid timezone'),
+    },
+    validateInputOnChange: true,
+  });
+
+  return (
+    <Box mx="auto">
+      <form onSubmit={form.onSubmit((values) => console.log(values))} onReset={form.onReset}>
+        <div className={styles.form__inputContainer}>
+          <TextInput
+            label="Meeting name"
+            placeholder="Meeting name"
+            {...form.getInputProps('meetingName')}
+          />
+        </div>
+        <div className={styles.form__datetimeContainer}>
+          <div>
+            <div>Range of possible dates for the meeting</div>
+            <DatePicker type="range" allowSingleDateInRange {...form.getInputProps('dateRange')} />
+          </div>
+          <div>
+            <Select
+              label="Start time"
+              placeholder="Pick value"
+              data={[...Array.from({ length: 24 }).map((_, index) => `${index}:00`)]}
+              {...form.getInputProps('startTime')}
+            />
+            <Select
+              label="End time"
+              placeholder="Pick value"
+              data={[...Array.from({ length: 24 }).map((_, index) => `${index}:00`)]}
+              {...form.getInputProps('endTime')}
+            />
+          </div>
+          <div className={styles.form__datetimeContainer}>
+            <Select
+              label="Your timezone"
+              placeholder="Type and select value"
+              data={[...timezones]}
+              searchable
+              {...form.getInputProps('timezone')}
+            />
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <button type="submit" className={styles.form__submit}>
+            Create meeting
+          </button>
+        </div>
+      </form>
+    </Box>
+  );
+}
