78ec326ec3a965171e8dfc70c8ccb099106082f3
theborgh <borghiborghi@gmail.com>
2023-12-16 21:43:39 +0100

    fetch meeting data on meeting page load

--- app/meeting/[id]/page.tsx
+++ app/meeting/[id]/page.tsx
@@ -1,5 +1,26 @@
+import { prisma } from '@/db';
+
 import React from 'react';
 
-export default function EditMeetingForm() {
-  return <div>Step two</div>;
+export default async function MeetingPage({ params }: { params: { id: string } }) {
+  let meetingData;
+
+  if (params.id) {
+    meetingData = await prisma.meeting.findUnique({
+      where: {
+        id: params.id,
+      },
+    });
+    console.log('fetched the meeting: ', meetingData);
+  }
+
+  return (
+    <div>
+      <h1>{meetingData?.name}</h1>
+      <div>
+        <div>Sign in form</div>
+        <div>Group availability (server component)</div>
+      </div>
+    </div>
+  );
 }
