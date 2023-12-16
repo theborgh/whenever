799596dfd484a650cc4ab6674036bd6089b68596
theborgh <borghiborghi@gmail.com>
2023-12-16 15:09:02 +0100

    add mock data

---
+++ app/test/mockData.ts
@@ -0,0 +1,82 @@
+export const mockMeeting = {
+  id: '1',
+  name: 'Test Meeting',
+  startDay: '2020-01-01',
+  endDay: '2020-01-02',
+  users: [
+    {
+      name: 'Test User',
+      timezone: 'America/New_York',
+      password: 'asd09sf8a7sdf',
+      availabilities: [
+        {
+          day: '2020-01-01',
+          startTime: '08:00',
+          endTime: '09:00',
+        },
+        {
+          day: '2020-01-01',
+          startTime: '10:00',
+          endTime: '11:00',
+        },
+        {
+          day: '2020-01-01',
+          startTime: '13:00',
+          endTime: '14:00',
+        },
+        {
+          day: '2020-01-02',
+          startTime: '08:00',
+          endTime: '09:00',
+        },
+        {
+          day: '2020-01-02',
+          startTime: '10:00',
+          endTime: '11:00',
+        },
+        {
+          day: '2020-01-02',
+          startTime: '13:00',
+          endTime: '14:00',
+        },
+      ],
+    },
+    {
+      name: 'Test User 2',
+      timezone: 'America/New_York',
+      password: 'asd09sf8a7sdf',
+      availabilities: [
+        {
+          day: '2020-01-01',
+          startTime: '08:00',
+          endTime: '09:00',
+        },
+        {
+          day: '2020-01-01',
+          startTime: '10:00',
+          endTime: '11:00',
+        },
+        {
+          day: '2020-01-01',
+          startTime: '13:00',
+          endTime: '14:00',
+        },
+        {
+          day: '2020-01-02',
+          startTime: '08:00',
+          endTime: '09:00',
+        },
+        {
+          day: '2020-01-02',
+          startTime: '10:00',
+          endTime: '11:00',
+        },
+        {
+          day: '2020-01-02',
+          startTime: '13:00',
+          endTime: '14:00',
+        },
+      ],
+    },
+  ],
+};
