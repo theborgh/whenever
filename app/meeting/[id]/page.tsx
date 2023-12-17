77f5b86d2571ba0fc3d9134d581fa2b01abe785c
theborgh <borghiborghi@gmail.com>
2023-12-17 08:53:34 +0100

    add react-selecto

--- app/meeting/[id]/page.tsx
+++ app/meeting/[id]/page.tsx
@@ -1,25 +1,28 @@
-import { prisma } from '@/db';
-
 import React from 'react';
+import { prisma } from '@/db';
+import DragSelectableCalendar from '@/components/DragSelectableCalendar/DragSelectableCalendar';
 
 export default async function MeetingPage({ params }: { params: { id: string } }) {
   let meetingData;
 
-  if (params.id) {
-    meetingData = await prisma.meeting.findUnique({
-      where: {
-        id: params.id,
-      },
-    });
-    console.log('fetched the meeting: ', meetingData);
-  }
+  // if (params.id) {
+  //   meetingData = await prisma.meeting.findUnique({
+  //     where: {
+  //       id: params.id,
+  //     },
+  //   });
+  //   console.log('fetched the meeting: ', meetingData);
+  // }
 
   return (
     <div>
-      <h1>{meetingData?.name}</h1>
+      {/* <h1>{meetingData?.name}</h1> */}
       <div>
         <div>Sign in form</div>
-        <div>Group availability (server component)</div>
+        <div>
+          <h2>Group availability (client component)</h2>
+          <DragSelectableCalendar />
+        </div>
       </div>
     </div>
   );
---
+++ components/DragSelectableCalendar/DragSelectableCalendar.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import Selecto from 'react-selecto';
+import './DragSelectableCalendar.css';
+
+export default function DragSelectableCalendar() {
+  const [container, setContainer] = useState<HTMLElement | null>(null);
+  const [selectableTargets, setSelectableTargets] = useState<(string | HTMLElement | null)[]>([]);
+
+  useEffect(() => {
+    setContainer(document.body);
+    setSelectableTargets(['.target', document.querySelector('.target2') as HTMLElement]);
+  }, []);
+
+  if (!container || !selectableTargets.length) return null;
+
+  return (
+    <div className="container">
+      <Selecto
+        // The container to add a selection element
+        container={document?.body}
+        // The area to drag selection element (default: container)
+        dragContainer={window}
+        // Targets to select. You can register a queryselector or an Element.
+        selectableTargets={['.element', document.querySelector('.target2') as HTMLElement]}
+        // Whether to select by click (default: true)
+        selectByClick={true}
+        // Whether to select from the target inside (default: true)
+        selectFromInside={true}
+        // After the select, whether to select the next target with the selected target (deselected if the target is selected again).
+        continueSelect={true}
+        // Determines which key to continue selecting the next target via keydown and keyup.
+        toggleContinueSelect={'shift'}
+        // The container for keydown and keyup events
+        keyContainer={window}
+        // The rate at which the target overlaps the drag area to be selected. (default: 100)
+        hitRate={0}
+        onSelect={(e) => {
+          e.added.forEach((el) => {
+            el.classList.toggle('selected');
+          });
+          e.removed.forEach((el) => {
+            el.classList.toggle('selected');
+          });
+        }}
+      />
+
+      <div id="elements-container" className="elements-container">
+        {Array.from({ length: 16 }, (_, i) => (
+          <div data-testid={`grid-cell-${i}`} key={i} className={`element`} />
+        ))}
+      </div>
+    </div>
+  );
+}
